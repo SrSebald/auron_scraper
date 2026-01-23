66a657427c5356746d157fff3ba4523a2e3795c9
Werner Sebald Cortez <we.sebald@duocuc.cl>
2026-01-23 12:27:53 -0300

    separating the function with the main c:

--- scrape_chess.py
+++
@@ -1,7 +0,0 @@
-'''Para que se importa re?'''
-import re 
-import time 
-import sqlite3
-from dataclasses import dataclass
-'''para que si'''
-from typing import
---
+++ src/ajefech_client.py
@@ -0,0 +1,54 @@
+ 
+
+'''Definition of the url where we are going to take the data.'''
+GRAPHQL_URL = ("https://www.federacionchilenadeajedrez.cl/graphql")
+'''Definir los headers para pedir el formato en el que vamos a extraer la información. 
+el user agent es para una identificación de que estamos haciendo
+'''
+header = {'Content-Type': 'application/json',
+'User-Agent': 'AJEFECH-Tournament-Scraper/1.0 (contact email: werner.sebald.c@gmail.com)'}
+
+
+"""Probando la Query para pedir la información a la página. las constantes son en mayusculas y las variables en minusculas."""
+query = "query ($word: String!) {\n  tournaments(word: $word) {\n    id\n    title\n    city\n    start_date: startDateFormated\n    address\n    __typename\n  }\n}"
+
+variables =  {"word": ""}
+
+payload = {
+    "query": query,
+    "variables": variables
+}
+
+"Logramos hacer funcionar el payload"
+
+print(type(payload))
+
+
+"""Creando la función necesaria para que reciba la variables."""
+def fetch_tournaments(word=""):
+    """Me esta costando pensar en la ejecución así que lo haré paso por paso"""
+
+    #1. darle hacer una varible con la información de word
+
+    # Listo 
+    variables = {"word": word}
+
+    
+    #2. Construir el Payload 
+    payload = {
+    "query": query,
+    "variables" : variables
+    }
+
+    #3. Hacer el Post 
+
+    #4. Validar estatus 
+
+    #5 JSON
+
+    #6 Validar errores 
+
+    #7.Extraer los torneos 
+
+    #8. Retornar los torneos
+    return tournaments
